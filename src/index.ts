d4da28bd13ff9ba934ff5113cc34b1b069d8b6ff
imtiaz <imtiazahmed026@gmail.com>
2024-03-02 01:47:29 -0600

    started with object

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log(" oop with typescript");
+class Animal {
+  name: string;
+  sound: string;
+  constructor(name: string, sound: string) {
+    this.name = name;
+    this.sound = sound;
+  }
+  makeSound() {
+    return `${this.name} says ${this.sound}`;
+  }
+  getName() {
+    return this.name;
+  }
+}
+const newAnimal1 = new Animal("cat", "meow");
+const getAnimalName = newAnimal1.getName();
+const animalSound = newAnimal1.makeSound();
+
+const newAnimal2 = new Animal("dog", "bark");
+const animalSound2 = newAnimal2.makeSound();
+const animalName2 = newAnimal2.getName();
