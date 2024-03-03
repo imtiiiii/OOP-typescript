afca999f50895db421b848bb2318a66acef5eb59
imtiaz <imtiazahmed026@gmail.com>
2024-03-02 20:34:45 -0600

    create new file object

---
+++ src/object.ts
@@ -0,0 +1,32 @@
+//* By default it is public */
+class Animal {
+  //*No need of below initializer when using access modifier  ex : public , private directly in constructor */
+  // name: string;
+  // sound: string;
+  // constructor(name: string, sound: string) {
+  //  this.name = name;
+  // this.sound = sound;
+  // }
+
+  //* Above code can be written as below */
+  constructor(
+    public name: string,
+    public sound: string,
+  ) {
+    this.name = name;
+    this.sound = sound;
+  }
+  makeSound() {
+    return `${this.name} says ${this.sound}`;
+  }
+  getName() {
+    return this.name;
+  }
+}
+const newAnimal1 = new Animal("cat", "meow");
+const getAnimalName = newAnimal1.getName();
+const animalSound = newAnimal1.makeSound();
+
+const newAnimal2 = new Animal("dog", "bark");
+const animalSound2 = newAnimal2.makeSound();
+const animalName2 = newAnimal2.getName();
