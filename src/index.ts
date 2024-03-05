e5fb8154bda342d2b339e2489e7d5c90345cbc23
imtiaz <imtiazahmed026@gmail.com>
2024-03-05 00:53:19 -0600

    polymorphism code

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,40 @@ class Shape {
   getArea() {}
 }
 class Circle extends Shape {
+  radius: number;
+  constructor(radius: number) {
+    super();
+    this.radius = radius;
+  }
   getArea() {
-    return 3.14 * 5 * 5;
+    return 3.14 * this.radius ** 2;
   }
 }
 class Square extends Shape {
+  side: number;
+  constructor(side: number) {
+    super();
+    this.side = side;
+  }
   getArea() {
-    return 5 * 5;
+    return this.side * this.side;
   }
 }
 class Rectangle extends Shape {
+  length: number;
+  width: number;
+  constructor(length: number, width: number) {
+    super();
+    this.length = length;
+    this.width = width;
+  }
   getArea() {
-    return 5 * 10;
+    return this.length * this.width;
   }
 }
+const circle = new Circle(5);
+const square = new Square(4);
+const rectangle = new Rectangle(5, 10);
+console.log(circle.getArea());
+console.log(square.getArea());
+console.log(rectangle.getArea());
